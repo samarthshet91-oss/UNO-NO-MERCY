766b35d492b1acfd47d4903b1ec2264a80a2a48c
Samarth S Shet <samarthshet91@gmail.com>
2026-04-11 11:52:49 +0530

    fix vite config for netlify

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,7 +3,6 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    allowedHosts: ["implicate-anchovy-contently.ngrok-free.dev"],
-  },
-});
+  base:"/",
+  }
+); 
