a303f5aa26435b9ff89367407b1bddcc4054adad
Samarth S Shet <samarthshet91@gmail.com>
2026-04-11 16:39:11 +0530

    fix alias for shared

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-
+import{ fileURLToPath,URL}from"node:url";  
 export default defineConfig({
   plugins: [react()],
-  base:"/",
+  resolve: {
+    alias: {
+      "@arc-blitz/shared": fileURLToPath(new URL("../shared/src", import.meta.url))
+    }
+  }
   }
 ); 
