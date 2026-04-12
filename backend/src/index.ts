174d0fedb4107e5142048c2e3bee56ec520a1bfe
Samarth S Shet <samarthshet91@gmail.com>
2026-04-12 12:37:29 +0530

    add vercel frontend url to cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -23,6 +23,7 @@ const allowedOrigins =[
   "http://127.0.0.1:5173",
   "http://localhost:5174",
   "http://127.0.0.1:5174",
+  "https://uno-no-mercy-frontend.vercel.app",
   "https://implicate-anchovy-contently.ngrok-free.dev",
 ];
 app.use(cors({
